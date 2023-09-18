b02d7ee64599577ae45dc89b3885ca2fb718f2e6
arathikg679 <145317570+arathikg679@users.noreply.github.com>
2023-09-18 15:49:17 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
             </div>
           </div>
           <div className="col w-100  text-center" id="home-bg">
-            <img src="/Bgimages/images1.webp" className="img w-100 shadow-lg" alt="home-image"  />
+            <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-book-club-illustration_23-2149334858.jpg.webp" className="img w-100 shadow-lg" alt="home-image"  />
           </div>
         </div>
       </div>
@@ -28,7 +28,7 @@ const Home = () => {
         <div className='display-6 text-center  pb-3' id="bst-head">Best Sellers</div>
         <div className='row g-5' id="best-sellers">
           <div className='col'>
-            <div className='card  w-100 shadow-lg'><img src="./Bgimages/hbook1.png" ></img></div>
+            <div className='card  w-100 shadow-lg'><img src="https://mir-s3-cdn-cf.behance.net/projects/404/a3f3cd85314781.Y3JvcCw4NTQsNjY4LDU0OSww.jpg" ></img></div>
             <div id="bestsel-text">
             <div ><h3>Aasha and the spirit bird</h3></div>
             <div className='lead'><p>$45</p></div>
@@ -36,7 +36,7 @@ const Home = () => {
           </div>
 
           <div className='col'>
-            <div className='card  w-100 shadow-lg'><img src="./Bgimages/hbook2.png" ></img></div>
+            <div className='card  w-100 shadow-lg'><img src="https://m.media-amazon.com/images/I/91f4Z+hyxiL._AC_UF1000,1000_QL80_.jpg" ></img></div>
             <div id="bestsel-text">
             <div ><h3>Okka bokka</h3></div>
             <div className='lead '><p>$100</p></div>
@@ -45,7 +45,7 @@ const Home = () => {
           </div>
 
           <div className='col'>
-            <div className='card  w-100 shadow-lg'><img src="./Bgimages/hbook3.webp" ></img></div>
+            <div className='card  w-100 shadow-lg'><img src="https://marketplace.canva.com/EAFMK67_WZk/1/0/1003w/canva-brown-and-cream-illustration-novel-book-cover-v3YbqY4UuWc.jpg" ></img></div>
             <div id="bestsel-text">
             <div ><h3>An old mother alone in the wilderness</h3></div>
             <div className='lead'><p>$50</p></div>
@@ -54,7 +54,7 @@ const Home = () => {
           </div>
 
           <div className='col'>
-            <div className='card  w-100 shadow-lg'><img src="./Bgimages/hbook4.jpg" ></img></div>
+            <div className='card  w-100 shadow-lg'><img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/summer-themed-children%27s-book-cover-design-template-8a6ac74063d879df4e8774dc960399b2_screen.jpg?ts=1637012075" ></img></div>
             <div id="bestsel-text">
             <div ><h3>The heroes of summer</h3></div>
             <div className='lead'><p>$80</p></div>
